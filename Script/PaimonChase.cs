dcc5656d37511e9df17b9b9d7b00ba8f06b812ad
izultatsumi <74152663+izultatsumi@users.noreply.github.com>
2020-11-30 00:38:40 +0700

    Add files via upload

---
+++ Script/PaimonChase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaimonChase : MonoBehaviour {
+
+    public Transform target;
+    float paimonSpeed = 0.5f;
+
+    const float tabrak = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.position);
+
+        if((transform.position - target.position).magnitude > tabrak)
+            transform.Translate(0f, 0f, paimonSpeed * Time.deltaTime);
+	}
+}
